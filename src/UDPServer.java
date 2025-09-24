a53eb4244ca9ce09cb55f1eeb1cf8d0713239744
Ben <Ben@10.220.45.233>
2025-09-24 16:11:05 -0400

    Contains the starter code for the UDPServer class, with TODO statements.

---
+++ src/UDPServer.java
@@ -0,0 +1,38 @@
+
+
+
+/**
+ * 
+ * @author Benjamin MacDougall, Sean Perez, Zach "", Alex ""
+ *
+ * @version 1.0 2025-09-24 Initial implementation
+ *
+ *
+ * @since 1.0
+ */
+public class UDPServer
+    {
+
+    /**
+     * 
+     * TODO Implement UDP server capabilities.
+     * 1) client --> server --> client  (server as the middleman)
+     * This means we need to make a queue system to handle multiple datagrams at a time.
+     * TODO Implement File I/O capabilities.
+     * This means sending files through a router and a server to another client where
+     * they can easily download and read files through their own file explorer.
+     * 
+     * TODO Decide if we want to do the middleman or just have one side be the server.
+     * 
+     *
+     * @since 1.0
+     */
+    public UDPServer()
+        {
+
+        // TODO Auto-generated constructor stub
+
+        }
+
+    }
+   // end class UDPServer
